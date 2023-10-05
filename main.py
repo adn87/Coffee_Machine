3ae7835d1d2f84c6645c436cdca18b95ecd6d883
adn87 <aaghadieh@gmail.com>
2023-10-05 13:57:32 -0400

    Coffee_machine_2
    - added comments
    - created a function to check if the user inserted enough money to buy a drink.
    - created a function to check if there are enough resources left for another order.
    - created a function to decrease the amount of resources after ordering a drink

--- main.py
+++ main.py
@@ -27,8 +27,7 @@ MENU = {
 resources = {
     "water": 300,
     "milk": 200,
-    "coffee": 100,
-    "money": 0
+    "coffee": 100
 }
 
 # TODO: ask the user what they would like (espresso/latte/cappuccino)
@@ -50,32 +49,65 @@ DIME = 0.10
 NICKLES = 0.05
 PENNIES = 0.01
 
+profit = 0
 
-def insert_coins(drink):
+
+def insert_coins():
+    """ returns the total amount coins inserted """
+
     print("Please insert coins.")
     quarters = int(input("How many quarters?: "))
     dimes = int(input("How many dimes?: "))
     nickles = int(input("How many nickles?: "))
     pennies = int(input("How many pennies?: "))
-    total = round(
-        (((QUARTER * quarters) + (DIME * dimes) + (NICKLES * nickles) + (PENNIES * pennies)) - MENU[drink]["cost"]), 2)
-    resources["money"] += total
+    total = ((QUARTER * quarters) + (DIME * dimes) + (NICKLES * nickles) + (PENNIES * pennies))
     return total
 
 
-order = True
-while order:
+def is_money_enough(payment, cost):
+    """returns True when the payment is accepted, or False if money is insufficient"""
+    global profit
+    if cost >= payment:
+        print(" Sorry that's not enough money. Money refunded.")
+        return False
+    else:
+        profit += cost
+        change = round(payment - cost, 2)
+        print(f"Here is ${change} in change.")
+        return True
+
+
+def resources_left(drink):
+    """Returns amount of resources left after ordering a drink"""
+    resources["water"] = resources["water"] - MENU[drink]["ingredients"]["water"]
+    if "milk" in MENU[drink]["ingredients"]:
+        resources["milk"] = resources["milk"] - MENU[drink]["ingredients"]["milk"]
+    resources["coffee"] = resources["coffee"] - MENU[drink]["ingredients"]["coffee"]
+    return resources
+
+
+def is_resource_sufficient(order_ingredients):
+    """Return True when order can be made. False if ingredients are insufficient"""
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there's not enough {item}")
+            return False
+    return True
+
+
+thirsty = True
+while thirsty:
     answer = input("what would you like? (espresso/latte/cappuccino): ").lower()
 
-    if answer == "espresso":
-        print(f"Here is ${insert_coins(answer)} in change. \nHere is your {answer} Enjoy!")
-    elif answer == "latte":
-        print(f"Here is ${insert_coins(answer)} in change. \nHere is your {answer} Enjoy!")
-    elif answer == "cappuccino":
-        print(f"Here is ${insert_coins(answer)} in change. \nHere is your {answer} Enjoy!")
-    elif answer == "report":
-        for key in resources:
-            print(f"{key}: {resources[key]}")
-    elif answer == "off":
+    if answer == "off":
         print(f"Coffee machine needs some rest. MACHINE {answer}")
-        order = False
+        thirsty = False
+    elif answer == "report":
+        print(f"Water: {resources["water"]}ml")
+        print(f"Milk: {resources["milk"]}ml")
+        print(f"Coffee: {resources["coffee"]}g")
+        print(f"money: ${profit}")
+    else:
+        if is_resource_sufficient(MENU[answer]["ingredients"]) and is_money_enough(insert_coins(), MENU[answer]["cost"]):
+            resources_left(answer)
+            print(f"Here is your {answer} Enjoy!")
